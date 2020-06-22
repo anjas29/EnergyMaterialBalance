55782108ae3b0256d8afe0044b8221bddf1835b3
Valeria <lera0349314@yandex.ru>
2020-06-22 13:53:41 +0300

    Refactored "CreatePoint"

--- EnergyAndMaterialBalanceModule/wwwroot/js/points/CreatePoints.js
+++ EnergyAndMaterialBalanceModule/wwwroot/js/points/CreatePoints.js
@@ -2,151 +2,42 @@
     ui: $('#createPointModal'),
     addButton: $('#btnCreatePointModal'),
     form: $('#createPointForm'),
+    modalMessage: $('#createPointMessage'),
+    collapse: $('#collapseCreatePoint'),
     validator: null,
     table: {
         ui: $('#createPointTagTable'),
-        fill: function (tags) {
-            var heads = $('#createPointTagTableHead');
+        selectButton: $('#selectCreatePointTag'),
+        fill: function (selectedSource, tags) {
+            var tdData;
+            var thData;
+            if (selectedSource === 1) {
+                tdData = CommonResources.tables.tags.body.iteh;
+                thData = CommonResources.tables.tags.head.iteh;
+            } else {
+                tdData = CommonResources.tables.tags.body.historian_manual;
+                thData = CommonResources.tables.tags.head.historian_manual;
+            }
 
-            heads.append(CreatePoints.table.head());
-
-            var rows = $('#createPointTagTableBody');
-            tags.forEach((tag, i) => {
-                rows.append(CreatePoints.table.row(tag, i));
-            });
+            Table.fillHead(this.ui, thData);
+            Table.fillBody(this.ui, tags, tdData);
         },
         clear: function () {
-            $('#createPointTagTableBody').html('');
-            $('#createPointTagTableHead').html('');
+            DataResponse.selectedTag = null;
+            CreatePoints.modal.tag.val('');
+            Table.clear(this.ui, true);
         },
-        head: function (selectedSource) {
-            var trData;
-            const tr = document.createElement('tr');
-            tr.className = 'd-flex';
-
-            if (DataResponse.selectedSource === '1') {
-                trData = [
-                    {
-                        col: 'col-1',
-                        data: '#'
-                    },
-
-                    {
-                        col: 'col-2',
-                        data: 'LSName'
-                    },
-
-                    {
-                        col: 'col-3',
-                        data: 'Имя базы данных'
-                    },
-
-                    {
-                        col: 'col-3',
-                        data: 'Имя таблицы'
-                    },
-
-                    {
-                        col: 'col-3',
-                        data: 'Имя тега'
-                    },
-                ];
-            } else {
-                trData = [
-                    {
-                        col: 'col-1',
-                        data: '#'
-                    },
-
-                    {
-                        col: 'col-4',
-                        data: 'Имя тега'
-                    },
-
-                    {
-                        col: 'col-7',
-                        data: 'Описание'
-                    },
-
-                ];
-            }
-
-            trData.forEach((thData) => {
-                const th = document.createElement('th');
-                th.append(thData.data);
-                th.className = thData.col;
-                tr.append(th);
-            });
-            return tr;
-        },
-        row: function (tag, i) {
-            var trData;
-            const tr = document.createElement('tr');
-            tr.className = 'd-flex';
-
-            if (DataResponse.selectedSource === '1') {
-                trData = [
-                    {
-                        col: 'col-1',
-                        data: i + 1
-                    },
-
-                    {
-                        col: 'col-2 lsname',
-                        data: tag.lsname
-                    },
-
-                    {
-                        col: 'col-3 dbname',
-                        data: tag.dbname
-                    },
-
-                    {
-                        col: 'col-3 tableName',
-                        data: tag.tableName
-                    },
-
-                    {
-                        col: 'col-3 tagName',
-                        data: tag.tagName
-                    },
-                ];
-            } else {
-                trData = [
-                    {
-                        col: 'col-1',
-                        data: i + 1,
-                    },
-
-                    {
-                        col: 'col-4 tagName',
-                        data: tag.tagName,
-                    },
-
-                    {
-                        col: 'col-7 description',
-                        data: tag.description,
-                    },
-
-                ];
-            }
-
-            trData.forEach((tdData) => {
-                const td = document.createElement('td');
-                td.append(tdData.data);
-                td.className = tdData.col;
-                tr.append(td);
-            });
-            return tr;
-        },
-
-        unselectRow: function () {
-            CreatePoints.table.ui.find('tr').removeClass('highlight');
+        unselectTag: function () {
+            Table.unselectRow(this.ui);
             DataResponse.selectedTag = null;
             CreatePoints.modal.tag.val(null);
         },
-    }
-    ,
+        selectTag: function (tr) {
+            DataResponse.selectedTag = $(tr).data('rowid');
+            CreatePoints.modal.tag.val(DataResponse.selectedTag);
+            Table.selectRow(this.ui, DataResponse.selectedTag);
+        }
+    },
     modal: {
         name: $('#createPointName'),
         validMistake: $('#createPointValidMistake'),
@@ -162,50 +53,44 @@
         });
         this.form.on('submit', function (e) {
             if (CreatePoints.form.valid()) {
-                CreatePoints.form.find(':input[type=submit]').prop('disabled', true);
+                Utils.submitDisabled(this, true);
+
                 e.preventDefault();
                 CreatePoints.createPoints();
             }
         });
         this.setValidator();
         this.modal.source.change(function () {
-            var sourceVal = $(this).val();
-            CreatePoints.modal.tag.prop('readonly', true);
-            CreatePoints.table.clear();
+            var sourceVal = parseInt($(this).val());
             DataResponse.selectedSource = sourceVal;
-            CreatePoints.modal.tag.val('');
 
-            if (sourceVal === '') {
-                Utils.buttonDisabled('#selectCreatePointTag', true);
-            }
-            else if (sourceVal === '1') {
-                Utils.buttonDisabled('#selectCreatePointTag', false);
-                CreatePoints.table.fill(DataResponse.tags.seicVMappingIteh);
-            }
-            else if (sourceVal === '2') {
-                Utils.buttonDisabled('#selectCreatePointTag', false);
-                CreatePoints.table.fill(DataResponse.tags.seicVMappingHistorian);
+            CreatePoints.table.clear();
+            CreatePoints.modal.tag.prop('readonly', true);
 
+            if (sourceVal === 1) {
+                Utils.buttonDisabled(CreatePoints.table.selectButton, false);
+                CreatePoints.table.fill(sourceVal, DataResponse.tags.seicVMappingIteh);
             }
-            else if (sourceVal === '3') {
-                Utils.buttonDisabled('#selectCreatePointTag', false);
-                CreatePoints.table.fill(DataResponse.tags.seicVMappingManual);
+            else if (sourceVal === 2) {
+                Utils.buttonDisabled(CreatePoints.table.selectButton, false);
+                CreatePoints.table.fill(sourceVal, DataResponse.tags.seicVMappingHistorian);
             }
-            else {
-                CreatePoints.modal.tag.prop('readonly', false);
-                Utils.buttonDisabled('#selectCreatePointTag', true);
-                $('#collapseCreatePoint').collapse('hide');
+            else if (sourceVal === 3) {
+                Utils.buttonDisabled(CreatePoints.table.selectButton, false);
+                CreatePoints.table.fill(sourceVal, DataResponse.tags.seicVMappingManual);
+            } else {
+                Utils.buttonDisabled(CreatePoints.table.selectButton, true);
+                CreatePoints.collapse.collapse('hide');
             }
         });
 
         this.table.ui.delegate('tr', 'click', function () {
 
             var selected = $(this).hasClass('highlight');
-            CreatePoints.table.unselectRow();
+            CreatePoints.table.unselectTag();
             if (!selected) {
                 $(this).addClass('highlight');
-                DataResponse.selectedTag = $(this).find(".tagName").html();
-                CreatePoints.modal.tag.val(DataResponse.selectedTag);
+                CreatePoints.table.selectTag(this);
             }
         });
 
@@ -215,20 +100,21 @@
         this.modal.name.val('');
         this.modal.validMistake.val('');
         this.modal.tag.val('');
-        Utils.populateSelect(this.modal.direction, null, Utils.directions, 'value', 'name');
+        Utils.populateSelect(this.modal.direction, null, CommonResources.directions, 'value', 'name');
         Utils.populateSelect(this.modal.period, null, DataResponse.periods, 'periodId', 'periodName');
         Utils.populateSelect(this.modal.source, null, DataResponse.sources, 'sourceId', 'sourceName');
         this.modal.bgroupId.val(DataResponse.selectedBGroup.bgroupId);
 
-        $('#collapseCreatePoint').collapse('hide');
+        CreatePoints.collapse.collapse('hide');
         CreatePoints.table.clear();
         DataResponse.selectedSource = null;
         DataResponse.selectedTag = null;
 
 
-        Utils.buttonDisabled('#selectCreatePointTag', true);
-        Utils.clearModalMessage('#createPointMessage');
-        this.form.find(':input[type=submit]').prop('disabled', false);
+        Utils.buttonDisabled(CreatePoints.table.selectButton, true);
+
+        Utils.clearModalMessage(this.modalMessage);
+        Utils.submitDisabled(this.form, false);
         this.validator.resetForm();
 
     },
@@ -242,9 +128,9 @@
         bgroupIdVal = this.modal.bgroupId.val();
 
         $.ajax({
-            method: "POST",
-            url: "/main/CreatePoint",
-            headers: { "Accept": "application/json", "Content-Type": "application/json" },
+            method: 'POST',
+            url: '/main/CreatePoint',
+            headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
             data: JSON.stringify({
                 bgroupId: bgroupIdVal,
                 direction: directionVal,
@@ -254,17 +140,19 @@
                 sourceId: sourceVal,
                 pointName: pointNameVal,
             })
-
         }).done(function (result) {
-            Utils.clearModalMessage('#createPointMessage');
-            $('#createPointModal').modal('hide');
+            Utils.clearModalMessage(CreatePoints.modalMessage);
+            CreatePoints.ui.modal('hide');
+
             DataResponse.selectedPoint = result.selectedPoint;
+
             TableView.addPoint(result.points, result.selectedPoint);
-            Message.show(false, "Точка учета '" + result.selectedPoint.pointName + "' была успешно добавлена!");
+
+            Message.show(false, Message.successes.createPointSuccess.format(result.selectedPoint.pointName));
         }).fail(function (result, textStatus) {
             if (textStatus !== 'abort') {
-                Utils.fillModalMessage('#createPointMessage', "Не удалось создать точку учета!");
-                CreatePoints.form.find(':input[type=submit]').prop('disabled', false);
+                Utils.fillModalMessage(CreatePoints.modalMessage, Message.errors.createPointError);
+                Utils.submitDisabled(CreatePoints.form, false);
             }
         });
     },
@@ -293,29 +181,7 @@
                 },
 
             },
-            messages: {
-                createPointName: {
-                    required: "Укажите имя точки учета, это поле не может быть пустым!",
-                    maxlength: "Название точки учета должно содержать меньше 255 символов!"
-                },
-                createPointValidMistake: {
-                    required: "Укажите допустимый дисбаланс, это поле не может быть пустым!",
-                    number: "Укажите допустимый дисбаланс в правильном формате!",
-                },
-                createPointDirection: {
-                    required: "Укажите направление, это поле не может быть пустым!",
-                },
-                createPointSource: {
-                    required: "Укажите источник, это поле не может быть пустым!",
-                },
-                createPointPeriod: {
-                    required: "Укажите период, это поле не может быть пустым!",
-                },
-                createPointTag: {
-                    required: "Укажите тег, это поле не может быть пустым!",
-
-                },
-            }
+            messages: Message.validation.createPoint,
         });
     }
 };
